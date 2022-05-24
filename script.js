ee7975c76cc8e12f51e575d356775995aff86bbf
srissirs <sarareis2002@gmail.com>
2022-05-24 12:26:13 +0100

    Items are being added/removed/altered in cart

--- script.js
+++ script.js
@@ -1,31 +1,109 @@
-function reviewsAndDishes(){
-    const restauranttoppage=document.querySelector("restauranttoppage")
-    const restaurant=document.querySelector("restaurant")
-    buttons=restauranttoppage.querySelectorAll("a")
-    dbutton=buttons[0]
-    rbutton=buttons[1]
-    dbutton.classList.add("selected")
-    var dishes=document.getElementById("dishes")
-    var reviews=document.getElementById("reviews")
-    reviews.remove()
-    dbutton.addEventListener('click',function(e) {
-      if(!dbutton.classList.contains("selected")){
-        dbutton.classList.toggle("selected")
-        rbutton.classList.toggle("selected")
-        reviews.remove()
-        restaurant.appendChild(dishes)
-      }  
-      
-      })
-    rbutton.addEventListener('click',function(e) {
-      if(!rbutton.classList.contains("selected")){
-        rbutton.classList.toggle("selected")
-        dbutton.classList.toggle("selected")
-        dishes.remove()
-        restaurant.appendChild(reviews)
-      }
-        
-      })
+function reviewsAndDishes() {
+  const restauranttoppage = document.querySelector("restauranttoppage")
+  const restaurant = document.querySelector("restaurant")
+  buttons = restauranttoppage.querySelectorAll("a")
+  dbutton = buttons[0]
+  rbutton = buttons[1]
+  dbutton.classList.add("selected")
+  var dishes = document.getElementById("dishes")
+  var reviews = document.getElementById("reviews")
+  reviews.remove()
+  dbutton.addEventListener('click', function (e) {
+    if (!dbutton.classList.contains("selected")) {
+      dbutton.classList.toggle("selected")
+      rbutton.classList.toggle("selected")
+      reviews.remove()
+      restaurant.appendChild(dishes)
+    }
+
+  })
+  rbutton.addEventListener('click', function (e) {
+    if (!rbutton.classList.contains("selected")) {
+      rbutton.classList.toggle("selected")
+      dbutton.classList.toggle("selected")
+      dishes.remove()
+      restaurant.appendChild(reviews)
+    }
+
+  })
 }
 
 reviewsAndDishes()
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "300px";
+  document.getElementById("main").style.marginRight = "-250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginRight = "0";
+}
+
+function removeDish() {
+  let dish = event.target.parentElement;
+  dish.remove();
+  showTotals();
+}
+
+function updateQuantity(quantity, name, price) {
+  const divs = document.querySelectorAll("cartDiv");
+  for (d of divs) {
+    if (d.textContent.includes(name)) {
+      const newQuantity = parseInt(quantity) + parseInt(d.children[3].textContent);
+      d.children[3].textContent = newQuantity;
+      showTotals();
+      return;
+    }
+  }
+}
+
+function addToCart() {
+
+  let name = event.target.parentElement.parentElement.children[0].children[0].textContent;
+  let price = event.target.parentElement.parentElement.children[1].textContent;
+  let quantity = event.target.parentElement.children[1].value;
+
+  if (document.getElementById('mySidebar').textContent.includes(name)) {
+    updateQuantity(quantity, name, price);
+    return;
+  }
+
+  if (quantity >= 1) {
+    const cartItem = document.createElement('cartDiv');
+    const nameNode = document.createElement("p");
+    const Name = document.createTextNode(name);
+    nameNode.appendChild(Name);
+    const priceNode = document.createElement("p");
+    const Price = document.createTextNode(price);
+    priceNode.appendChild(Price);
+    const quantityNode = document.createElement("p");
+    const Quantity = document.createTextNode(quantity);
+    quantityNode.appendChild(Quantity);
+    const html = '<a href="javascript:void(0)" class="removeDish" onclick="removeDish()">&times;</a>';
+    cartItem.insertAdjacentHTML('afterbegin', html);
+    cartItem.appendChild(nameNode);
+    cartItem.appendChild(priceNode);
+    cartItem.appendChild(quantityNode);
+    const cart = document.getElementById('mySidebar');
+    const total = document.getElementById('totalSum');
+    cart.insertBefore(cartItem, total);
+    showTotals();
+  }
+}
+
+
+function showTotals(){
+  let total = 0;
+  let quantity, price;
+  const divs = document.querySelectorAll("cartDiv");
+  for (d of divs) {
+    quantity = parseInt(d.children[3].textContent);
+    price = parseFloat(d.children[2].textContent);
+    total += parseFloat(quantity*price,2);
+    
+  }
+  const cart = document.getElementById('totalSum');
+  cart.textContent="Total: "+total;
+}
--- templates/restaurant.tpl.php
+++ templates/restaurant.tpl.php
@@ -1,47 +1,65 @@
-<?php 
-  declare(strict_types = 1); 
+<?php
 
-    require_once('database/connection.database.php');
+declare(strict_types=1);
 
-  require_once('database/restaurant.class.php');
-  require_once('database/review.class.php');
-  require_once('database/reviewResponse.class.php');
+require_once('database/connection.database.php');
+
+require_once('database/restaurant.class.php');
+require_once('database/review.class.php');
+require_once('database/reviewResponse.class.php');
 ?>
 
-<?php function drawRestaurants(array $restaurants) { ?>
+<?php function drawRestaurants(array $restaurants)
+{ ?>
   <h2>Restaurants</h2>
   <section id="restaurants">
-    <?php foreach($restaurants as $restaurant) { ?> 
+    <?php foreach ($restaurants as $restaurant) { ?>
       <article>
-        <a href="restaurant.php?id=<?=$restaurant->id?>"><?=$restaurant->restaurantName?></a>
+        <a href="restaurant.php?id=<?= $restaurant->id ?>"><?= $restaurant->restaurantName ?></a>
       </article>
     <?php } ?>
   </section>
 <?php } ?>
 
-<?php function drawRestaurantHeader(Restaurant $restaurant) { ?>
+<?php function drawRestaurantHeader(Restaurant $restaurant)
+{ ?>
   <restaurantHeader>
-      <restaurantInfo>
-  <h2><?=$restaurant->restaurantName?></h2>
-  <h3> 
-          <i class="fa-regular fa-star"></i>
-          <i class="fa-regular fa-star"></i>
-          <i class="fa-regular fa-star"></i>
-          <i class="fa-regular fa-star"></i>
-          <i class="fa-regular fa-star"></i>
-        </h3> 
-        <h4> <?=$restaurant->restaurantAddress?> </h4> 
-  </restaurantInfo>
-</restaurantHeader>
+    <restaurantInfo>
+      <h2><?= $restaurant->restaurantName ?></h2>
+      <h3>
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+      </h3>
+      <h4> <?= $restaurant->restaurantAddress ?> </h4>
+    </restaurantInfo>
+  </restaurantHeader>
 <?php } ?>
 
-<?php function drawRestaurant(Restaurant $restaurant, array $dishes, array $reviews) { ?>
-<restaurant>
-  <restaurantTopPage>
-      
-        <a > Dishes </a>
-        <a> Reviews </a>
-      
+<?php function drawRestaurant(Restaurant $restaurant, array $dishes, array $reviews)
+{ ?>
+  <restaurant>
+    <restaurantTopPage>
+
+      <a> Dishes </a>
+      <a> Reviews </a>
+
+      <div id="mySidebar" class="sidebar">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <p> Cart </p>
+       
+        <total class="totalSum" id="totalSum">Total : </total>
+      </div>
+
+      <div id="mySidenav" class="sidenav">
+        <a href="#" id="cart" class="openbtn" onclick="openNav()">
+          <i class="fa-solid fa-cart-shopping"></i>
+          <p>Cart</p>
+        </a>
+      </div>
+
       <form action="#">
         <select name="languages" id="lang" onchange="this.form.submit();">
           <option value="Chinese">Chinese</option>
@@ -49,56 +67,56 @@
         </select>
       </form>
     </restaurantTopPage>
-  <section id="dishes">
-    <?php foreach ($dishes as $dish) { ?>
-              <dish>
-                
-                <img src="https://picsum.photos/200?1" alt="Dish Photo">
-                
-                <information>
-                  <name>
-                    <p id="name"> <?=$dish->dishName?> </p>
-                    <i class="fa-regular fa-star"></i>
-                  </name>
+    <section id="dishes">
+      <?php foreach ($dishes as $dish) { ?>
+        <dish>
 
-                  <price>
-                      <p id="price"> <?=$dish->dishPrice?> </p>
+          <img src="https://picsum.photos/200?1" alt="Dish Photo">
 
-                    <i class="fa-solid fa-cart-shopping"></i>
-                  </price>
-                </information>
-              </dish> 
-              
-            
-   
-</restaurant>
-    <?php } ?>
-  </section>
+          <information>
+            <name>
+              <p id="name"> <?= $dish->dishName ?> </p>
+              <i class="fa-regular fa-star"></i>
+            </name>
+            <p id="price"> <?= $dish->dishPrice ?> </p>
+            <price>
+              <label for="quantity">Quantity:</label>
+              <input type="number" id="quantity" name="quantity" min="1" max="20">
+              <button class="fa-solid fa-cart-shopping button" onclick="addToCart(),openNav()"></button>
+            </price>
+          </information>
+        </dish>
 
-   <section id="reviews">
-    <?php foreach ($reviews as $review) {
-      $db = getDatabaseConnection();
-      $response = ReviewResponse::getReviewResponse($db, intval($review->reviewId));
-       ?>
-        <reviewBox>
-                <review>
-                    <info>
-                        <p id="name"> <?=ReviewRestaurant::getReviewerName($db, intval($review->customerId))?>  </p>
-                        <h3> 
-                            <i class="fa-regular fa-star"></i>
-                            <i class="fa-regular fa-star"></i>
-                            <i class="fa-regular fa-star"></i>
-                            <i class="fa-regular fa-star"></i>
-                            <i class="fa-regular fa-star"></i>
-                        </h3> 
-                    </info>
-                    <p id="reviewBody"> <?=$review->reviewText?>  </p>
-                </review>                
-                <p id="response"> Resposta: <?= $response->reviewText?> </p>
 
-            </reviewBox>
-</restaurant>
-    <?php } ?>
-  </section>
+
+  </restaurant>
+<?php } ?>
+</section>
+
+<section id="reviews">
+  <?php foreach ($reviews as $review) {
+    $db = getDatabaseConnection();
+    $response = ReviewResponse::getReviewResponse($db, intval($review->reviewId));
+  ?>
+    <reviewBox>
+      <review>
+        <info>
+          <p id="name"> <?= ReviewRestaurant::getReviewerName($db, intval($review->customerId)) ?> </p>
+          <h3>
+            <i class="fa-regular fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+          </h3>
+        </info>
+        <p id="reviewBody"> <?= $review->reviewText ?> </p>
+      </review>
+      <p id="response"> Resposta: <?= $response->reviewText ?> </p>
+
+    </reviewBox>
+    </restaurant>
+  <?php } ?>
+</section>
 
 <?php } ?>
