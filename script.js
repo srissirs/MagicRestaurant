2b578f0e373143640729199924944a46a479ab71
Teresa Ferreira <teresa.t.ferreira@hotmail.com>
2022-05-30 17:08:47 +0100

    signup

--- script.js
+++ script.js
@@ -28,4 +28,79 @@ function reviewsAndDishes(){
       })
 }
 
+
+function addDishes(a,count){
+  
+  var dishes=document.getElementById("dishes")
+  for (let i = 0; i < count; i++) {
+    dishes.appendChild(a[i])
+  }
+}
+
+
+function filter(){
+  const allDishes=document.querySelectorAll("dish")
+  m=NodeList
+  count=0
+  for (let i = 0; i < allDishes.length; i++) {
+    m[i]=allDishes[i]
+    count++
+  }
+  
+  const restaurant=document.querySelector("restaurant")
+  var dropdown=document.querySelector("select")
+  dropdown.addEventListener('change',function(e) {
+    var dishes=document.getElementById("dishes")
+    if(dropdown.value!="Tudo"){
+      dishes.remove
+      addDishes(m,count)
+      var dish=document.querySelectorAll("dish")
+      dish.forEach( el => {
+        c=el.querySelector("information")
+        d=c.querySelector("category")
+        e=d.querySelector("p")
+        l=e.textContent
+        if(l != " "+dropdown.value+" "){
+          el.remove()
+        }
+      })
+
+    }else{
+      dishes.remove
+      addDishes(m,count)
+    }
+      
+    })
+  
+
+  ;
+}
+
+function drawStar(){
+  var x=document.querySelector("h3")
+  y=x.querySelectorAll("i")
+  z=x.querySelector("p")
+  console.log(z.textContent)
+  z.textContent=Math.round(z.textContent)
+  if(z.textContent>=1){
+    y[0].classList.add("full")
+  }
+  if(z.textContent>=2){
+    y[1].classList.add("full")
+  }
+  if(z.textContent>=3){
+    y[2].classList.add("full")
+  }
+  if(z.textContent>=4){
+    y[3].classList.add("full")
+  }
+  if(z.textContent==5){
+    y[4].classList.add("full")
+  }
+}
+
+drawStar()
+
 reviewsAndDishes()
+
+filter()
