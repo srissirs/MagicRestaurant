cf70e16791abb0b3a3280ed06673067bbfc969b9
Teresa Ferreira <teresa.t.ferreira@hotmail.com>
2022-06-14 10:35:18 +0100

    sign up and sign in css

--- pages/signup.php
+++ pages/signup.php
@@ -7,9 +7,10 @@
     <script src="https://kit.fontawesome.com/e93bc86ff0.js" crossorigin="anonymous"></script>
   </head>
   <body id="loginPage">
-    <signUp>
-      <?php include_once('session.php');?>
+    <section id="Signup">
+
       <form action="../actions/action_signup.php" method="post" class="register_form">
+      <div id="input">
         <input type="text" id="firstname" name="firstname" placeholder="First Name" required="required"><br>
         <input type="text" id="lastname" name="lastname" placeholder="Last Name" required="required"><br>
         <input type="text" id="username" name="username" placeholder="Username" required="required"><br>
@@ -20,16 +21,22 @@
         <input type="number" id="phone" name="phone" placeholder="Phone Number" required="required"><br>
         <input type="email" id="email" name="email" placeholder="Email" required="required"><br>
         <input id="password" name="password" type="password" placeholder="Password" required="required"><br>
-        <div>  
+      </div>
+      <div id="check">
+        <div id="restaurantOwner">  
         <label for="ImRestaurant">I'm a restaurant owner</label>
         <input type="checkbox" id="restaurantowner" name="restaurantowner" value=1>
         </div>
         <button type="submit"> Register </button><br>
-      </form>
-        <p id="error_messages" style="color: black"> 
-        
-           <?php if(isset($_SESSION['ERROR'])) echo htmlentities($_SESSION['ERROR']); unset($_SESSION['ERROR'])?>
+        <a href="signin.php">Sign in</a> <br>
+        <p id="error_messages" style="color: #946B6B">
+        <?php include_once('../session.php');?>
+           <?php if(isset($_SESSION['ERROR'])) {echo htmlentities($_SESSION['ERROR']); unset($_SESSION['ERROR']);}?>
         </p>
-    </signUp>
+      </div>
+      </form>
+      
+        
+</section>
   </body>
     
