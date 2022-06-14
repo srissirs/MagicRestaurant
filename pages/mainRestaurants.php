3234e4ed8d3d5f8f582ff231ed3503367fdebca8
Teresa Ferreira <teresa.t.ferreira@hotmail.com>
2022-06-14 15:00:08 +0100

    corrected dish construct

--- pages/mainRestaurants.php
+++ pages/mainRestaurants.php
@@ -12,7 +12,7 @@
   require_once(__DIR__.'/../templates/common.tpl.php');
   require_once(__DIR__.'/../templates/mainRestaurants.tpl.php');
 
-
+ if (!isset($_SESSION['userId'])) header('Location: signin.php');
 
   $db = getDatabaseConnection();
   
